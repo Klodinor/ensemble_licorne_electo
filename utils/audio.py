6c42bee975523464ac8b73258ec47997934fbcad
Alexandre Tessier <alexandre_t@live.ca>
2019-03-12 16:53:03 -0400

    Instruments fonctionne dans UI

--- utils/audio.py
+++ utils/audio.py
@@ -14,22 +14,22 @@ class Audio:
         self._server.amp = 0.1
         
         #Test vent feuilles
-        ventTest = VentFeuilles()
-        ventTest.out()
+        self.ventTest = VentFeuilles()
+        self.ventTest.out()
         
         #Test feu
         #fire-all
-        feuToFilter = Feu()
-        feu01 = ButBP(feuToFilter.out(), freq=600, q=0.2, mul=0.5).mix(2).out() 
-        feu02 = ButBP(feuToFilter.out(), freq=1200, q=0.6, mul=0.5).mix(2).out()
-        feu03 = ButBP(feuToFilter.out(), freq=2600, q=0.4, mul=0.5).mix(2).out() 
-        feu04 = Atone(feuToFilter.out(), freq=1000, mul=0.5).mix(2).out()
+        self.feuToFilter = Feu()
+        feu01 = ButBP(self.feuToFilter.out(), freq=600, q=0.2, mul=0.5).mix(2).out() 
+        feu02 = ButBP(self.feuToFilter.out(), freq=1200, q=0.6, mul=0.5).mix(2).out()
+        feu03 = ButBP(self.feuToFilter.out(), freq=2600, q=0.4, mul=0.5).mix(2).out() 
+        feu04 = Atone(self.feuToFilter.out(), freq=1000, mul=0.5).mix(2).out()
         
         #test pluie
-        pluieMatTest = PluieMateriaux()
+        self.pluieMatTest = PluieMateriaux().out()
 
         #self.freqPort = SigTo(value=440, time=0.05, init=250)
-        self.sineTest = Sine(freq=440, mul=0.3).mix(2).mix(2).out()
+        #self.sineTest = Sine(freq=440, mul=0.3).mix(2).mix(2).out()
 
     def startServer(self, state):
         if state:
